94723518bfcc7d3f60f8f94a694ae24eea0f9bd8
Simo-Pekka Koskinen <simo.pekka.koskinen@gmail.com>
2015-11-08 23:49:18 +0200

    Added (most) of the content and added first style tweaks

--- src/client/entry.js
+++ src/client/entry.js
@@ -2,8 +2,12 @@ import React from "react";
 import App from "../shared/components/App";
 
 require("./stylesheets/styles.css");
-require("file!./images/profile_pic.jpg");
+
 
 const mountNode = document.getElementById('app');
 
 React.render(<App />, mountNode);
+
+var image = document.getElementById('profilePic')
+image.src = require("file!./images/profile_pic.jpg");
+//image.height = "100px";
--- src/shared/components/App.js
+++ src/shared/components/App.js
@@ -2,7 +2,12 @@ import React from "react";
 
 let App = class App extends React.Component {
   render() {
-    return <Resume />;
+    return (
+      <div class="container">
+        <Resume />
+        <Footer/>
+      </div>
+    );
   }
 }
 
@@ -11,6 +16,7 @@ let Resume = React.createClass({
     return (
       <div id="resume" className="resume">
         <ResumeHeader />
+        <ResumeBody />
       </div>
     );
   }
@@ -21,7 +27,7 @@ let ResumeHeader = React.createClass({
     return (
       <div>
         <div className="resume-header-info">
-          <div className="row">
+          <div className="row resume-header">
             <div className="row-cell-left">Curriculum Vitae</div>
             <div className="row-cell-right">7.11.2015</div>
           </div>
@@ -38,7 +44,7 @@ let ResumeHeader = React.createClass({
 
         </div>
         <div className="resume-profile-picture">
-          <img src="profile_pic.jpg"/>
+          <img id="profilePic" height="300" width="300"/>
         </div>
       </div>
     );
@@ -47,49 +53,117 @@ let ResumeHeader = React.createClass({
 
 let ResumeBody = React.createClass({
   render() {
-    <div>
-      <ResumeSection header="Education">
-        <HeadingRow leftContent="2006-2015" rightContent="Master of Science in Engineering, University of Turku"/>
-        <DetailRow content="Major: Software engineering, Minor: business knowledge" />
-        <DetailRow content="Thesis: Adopting automatic acceptance testing" />
-        <HeadingRow leftContent="2002-2005" rightContent="Graduate, Halikon lukio"/>
-      </ResumeSection>
-    </div>
+    return (
+      <div>
+        <ResumeSection header="Education">
+          <HeadingRow leftContent="2006-2015" rightContent="Master of Science in Engineering, University of Turku"/>
+          <DetailRow rowContent="Major: Software engineering, Minor: business knowledge" rowCellClass="row-cell-right"/>
+          <DetailRow rowContent="Thesis: Adopting automatic acceptance testing" rowCellClass="row-cell-right"/>
+          <HeadingRow leftContent="2002-2005" rightContent="Graduate, Halikon lukio"/>
+        </ResumeSection>
+        <ResumeSection header="Work History">
+          <HeadingRow leftContent="2009/09-present" rightContent="Cerion Solutions Oy"/>
+          <DetailRow rowContent="As a technical consultant I have designed, implemented and maintained several web-based budgeting and strategy planning applications and customer portals." rowCellClass="row-cell-right"/>
+          <HeadingRow leftContent="2008/05-2008/8" rightContent="Nokia Oyj"/>
+          <DetailRow rowContent="Assempbly line worker" rowCellClass="row-cell-right"/>
+          <HeadingRow leftContent="2007/07-2007/8" rightContent="Nokia Oyj"/>
+          <DetailRow rowContent="Assempbly line worker" rowCellClass="row-cell-right"/>
+          <HeadingRow leftContent="2005/05-2006/8" rightContent="Nokia Oyj"/>
+          <DetailRow rowContent="Assempbly line worker" rowCellClass="row-cell-right"/>
+        </ResumeSection>
+        <ResumeSection header="Technical skills">
+          <HeadingRow leftContent="Languages" rightContent=""/>
+          <DetailRow rowContent="Java, Scala, Javascript, Coffeescript" rowCellClass="row-cell-right"/>
+          <HeadingRow leftContent="Platforms" rightContent=""/>
+          <DetailRow rowContent="Java EE, Node.js, Force.com" rowCellClass="row-cell-right"/>
+          <HeadingRow leftContent="Front-end libraries" rightContent=""/>
+          <DetailRow rowContent="React.js, Angular.js, Backbone.js" rowCellClass="row-cell-right"/>
+          <HeadingRow leftContent="Back-end frameworks" rightContent=""/>
+          <DetailRow rowContent="Play Framework, Tapestry 5, JSF 2" rowCellClass="row-cell-right"/>
+          <HeadingRow leftContent="Test tools" rightContent=""/>
+          <DetailRow rowContent="Cucumber, JUnit" rowCellClass="row-cell-right"/>
+        </ResumeSection>
+        <ResumeSection header="Languages">
+          <DetailRow rowContent="Finnish - native proficiency" rowCellClass="row-cell-left"/>
+          <DetailRow rowContent="English - professional working proficiency" rowCellClass="row-cell-left"/>
+          <DetailRow rowContent="Swedish - limited proficiency" rowCellClass="row-cell-left"/>
+          <DetailRow rowContent="German - elementary proficiency" rowCellClass="row-cell-left"/>
+        </ResumeSection>
+        <ResumeSection header="Certifications">
+          <DetailRow rowContent="Functional Programming Principles in Scala - Coursera" rowCellClass="row-cell-left"/>
+          <DetailRow rowContent="Principles of Reactive Programming - Coursera" rowCellClass="row-cell-left"/>
+        </ResumeSection>
+        <ResumeSection header="Volunteer experience">
+          <HeadingRow leftContent="2010" rightContent="Vice-Chairman, Digit ry"/>
+          <HeadingRow leftContent="2007-2009" rightContent="Director of Recreational Activities, Digit ry"/>
+          <HeadingRow leftContent="2009-2014" rightContent="Organizer, Turku Agile Day"/>
+        </ResumeSection>
+        <ResumeSection header="Intrests">
+          <TextAreaRow textAreaContent="In spare time I enjoy programming. I love to try out new technologies, work with small private applications and complete online courses and tutorials. I also enjoy sports. I used to play basketball, but nowadays I jog and play various team sports. In the winter I love to go snowboarding, either in Finland or abroad." />
+          </ResumeSection>
+      </div>
+    );
   }
-}):
+});
+
+let Footer = React.createClass({
+    render() {
+      return (
+        <p className="credits">View Sources in <a href="https://github.com/Pasadena/react-resume" target="_blank">Github</a></p>
+      );
+    }
+});
 
 let ResumeSection = React.createClass({
   render() {
-    <div>
-      <div className="resume-section-header">
-        {this.props.header}}
+    return (
+      <div className="resume-section">
+        <div className="resume-section-header">
+          {this.props.header}
+        </div>
+        {this.props.children}
       </div>
-      {this.props.children}
-    </div>
+    );
   }
-}):
+});
 
 let HeadingRow = React.createClass({
   render() {
-    <div className="row row-header">
-      <div className="row-cell-left">
-        {this.props.leftContent}
+    return (
+      <div className="row row-header">
+        <div className="row-cell-left">
+          {this.props.leftContent}
+        </div>
+        <div className="row-cell-right">
+          {this.props.rightContent}
+        </div>
       </div>
-      <div className="row-cell-right">
-        {this.props.rightContent}
-      </div>
-    </div>
+    );
   }
-}):
+});
 
 let DetailRow = React.createClass({
   render() {
-    <div className="row">
-      <div className="row-cell-right">
-        {this.props.content}
+    return (
+      <div className="row">
+        <div className={this.props.rowCellClass}>
+          {this.props.rowContent}
+        </div>
       </div>
-    </div>
+    );
   }
-}):
+});
+
+let TextAreaRow = React.createClass({
+  render() {
+    return (
+      <div className="row">
+        <pre>
+          {this.props.textAreaContent}
+        </pre>
+      </div>
+    );
+  }
+});
 
 module.exports = App;
