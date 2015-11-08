807f07acc43c40d8967bbb5425445c2fdcc2985d
Simo-Pekka Koskinen <simo.pekka.koskinen@gmail.com>
2015-11-09 00:42:44 +0200

    Modified add to listen to correct port on heroku deploy

--- src/server/server.js
+++ src/server/server.js
@@ -16,8 +16,8 @@ app.get('/*', function (req, res) {
   res.render('index', { content: content });
   //});
 });
-
-var server = app.listen(3000, function () {
+var port = process.env.PORT || 3000;
+var server = app.listen(port, function () {
 
   var host = server.address().address;
   var port = server.address().port;
